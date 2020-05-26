05b391e0577afa516ba1b7f42890b4e71316c55f
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-26 13:09:32 +0300

    ED4d

---
+++ story_content/user.js
@@ -0,0 +1,55 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6mV5Rs95sqs":
+        Script1();
+        break;
+      case "5ar8zwccMoV":
+        Script2();
+        break;
+      case "6XmlxPmUDcv":
+        Script3();
+        break;
+      case "6VPchoLwW8K":
+        Script4();
+        break;
+      case "6R6qQ4hkYqd":
+        Script5();
+        break;
+      case "6lepbDXhCjN":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script6()
+{
+  window.print();
+}
+
